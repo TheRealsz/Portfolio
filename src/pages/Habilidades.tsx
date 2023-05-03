c14138d9ee0244a1f650004a80f47993276b4a67
unknown <Robindiegoa@gmail.com>
2023-05-03 02:06:00 -0300

    GithubCalendar modificado e traducao na pagina de habilidades adicionado

--- src/pages/Habilidades.tsx
+++ src/pages/Habilidades.tsx
@@ -5,19 +5,19 @@ import { Card } from "../components/Card";
 import { SiReact, SiTypescript, SiHtml5, SiCss3, SiBootstrap, SiSass, SiJavascript, SiMysql, SiGit, 
          SiGithub, SiPython, SiFigma, SiVisualstudiocode, SiWindows, SiNpm } from 'react-icons/si'
 import { TbJson } from 'react-icons/tb'
-import GitHubCalendar from 'react-github-calendar'
+import { useTranslation } from "react-i18next"
 
-
-// dias que codei API GITHUB
 export function Habilidades() {
-
+    
+    const { t } = useTranslation()
+    
     return (
         <section>
             <Container fluid id="defaultContainer">
                 <Particle />
                 <Container className="skills">
                     <div className="title">
-                        <h1><span>Habilidades </span> profissionais</h1>
+                        <h1><span>{t("Habilidade")}s </span> {t("profissionais")}</h1>
                     </div>
                     <Row className="cardRow">
                         <Col md={3}>
@@ -84,7 +84,7 @@ export function Habilidades() {
                 </Container>
                 <Container className="tools">
                     <div className="title">
-                        <h1><span>Ferramentas </span>que uso</h1>
+                        <h1>{t("Minhas")} <span>{t("ferramentas")}</span></h1>
                     </div>
                     <Row className="cardRow">
                         <Col md={3}>
@@ -107,11 +107,6 @@ export function Habilidades() {
                                 <SiFigma />
                             </Card>
                         </Col>
-                    </Row>
-                </Container>
-                <Container className="calender-github">
-                    <Row>
-                        <GitHubCalendar username="TheRealRobinho" fontSize={18} blockSize={17} blockMargin={6}/>
                     </Row>
                 </Container>
             </Container>
--- src/pages/Projetos.tsx
+++ src/pages/Projetos.tsx
@@ -1,7 +1,40 @@
+import GitHubCalendar from "react-github-calendar";
+import {ThemeInput , Activity} from "react-activity-calendar";
 import Particle from "../components/Particle";
+import { Container, Row, Col } from "react-bootstrap"
 
-export function Projetos(){
-    return(
-        <Particle/>
+export function Projetos() {
+
+    const explicitTheme: ThemeInput = {
+        light: ['#fff', '#0F516C']
+      }; 
+  
+      const selectLastHalfYear = (contributions: Activity[]) => {
+          const currentYear = new Date().getFullYear();
+          const currentMonth = new Date().getMonth();
+          const shownMonths = 6;
+        
+          return contributions.filter((activity: Activity) => {
+            const date = new Date(activity.date);
+            const monthOfDay = date.getMonth();
+        
+            return (
+              date.getFullYear() === currentYear &&
+              monthOfDay > currentMonth - shownMonths &&
+              monthOfDay <= currentMonth
+            );
+          });
+        };
+
+    return (
+        <Container fluid id="defaultContainer">
+            <Particle />
+            <Container className="calender-github">
+                <Row>
+                    <GitHubCalendar transformData={selectLastHalfYear} username="TheRealRobinho" colorScheme="light" fontSize={17} blockSize={17} style={{ color: '#fff' }} theme={explicitTheme} blockMargin={6} />
+                </Row>
+            </Container>
+        </Container>
+
     )
 }
