67452a1d0002018c70808ed6940fdb7387fc21fc
unknown <Robindiegoa@gmail.com>
2023-05-16 14:27:12 -0300

    mudancas do design dos project cards

--- src/components/ProjectCard.tsx
+++ src/components/ProjectCard.tsx
@@ -1,32 +1,31 @@
 import { Button } from "react-bootstrap";
 import { AiFillGithub } from "react-icons/ai";
 import { ReactNode } from 'react'
+import Ada from '../assets/Ada.png'
 import '../styles/projectCard.scss'
 
-type projectCardProps = {
-    image: string,
-    titulo: string,
-    descricao: string,
-    urlGithub: string,
-    urlDemo?: string,
-    disabled?: boolean
-}
+// type projectCardProps = {
+//     image: string,
+//     titulo: string,
+//     descricao: string,
+//     urlGithub: string,
+//     urlDemo?: string,
+//     disabled?: boolean
+// }
 
-export function ProjectCard(props : projectCardProps){
+
+export function ProjectCard(){
     return (
-        <div className="project-card">
-        <div className="lines"></div>
-        <div className="img-project">
-            <img src={props.image} alt="" />
+    <div className="project-card">
+        <div className="imgProject">   
+            <img src={Ada} alt="" />
         </div>
-        <div className="content">
-            <div className="resume">
-                <h2>{props.titulo}</h2>
-                <p>{props.descricao}</p>
-                <div className="buttons-card">
-                    <Button size="sm" className="buttonCard" href={props.urlGithub} target="blank_"> <AiFillGithub /> Github</Button>
-                    <Button size="sm" className="buttonCard" href={props.urlDemo} disabled={props.disabled} target="blank_">Demo</Button>
-                </div>
+        <div className="content">            
+                <h2>Ada</h2>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates minima esse harum blanditiis, sapiente voluptatum aut excepturi vel saepe nam et magnam nobis tempore ipsum repudiandae accusantium inventore placeat reprehenderit!</p>
+            <div className="buttons-card">
+                <Button size="sm" className="buttonCard">GitHub</Button>
+                <Button size="sm" className="buttonCard">Demo</Button>
             </div>
         </div>
     </div>
--- src/pages/Projetos.tsx
+++ src/pages/Projetos.tsx
@@ -7,10 +7,9 @@ import LOL from '../assets/lolLogo.png'
 import ListEfficient from '../assets/ListEfficient.png'
 import DateCountdown from '../assets/Date.png'
 import IMC from '../assets/IMC.png'
+import Ada from '../assets/Ada.png'
 import '../styles/projects.scss'
 
-// Todos os projetos, até o CRUD e Figma
-// Made with?
 
 export function Projetos() {
 
@@ -48,40 +47,7 @@ export function Projetos() {
             <Container className="projects">
                 <Row>
                     <Col md={4}>
-                        <ProjectCard
-                            image={ListEfficient}
-                            titulo="List Efficient"
-                            descricao="List Efficient é um projeto destinado a todos que buscam organizar suas tarefas seja do dia a dia, do trabalho, da faculdade ou de qualquer outro ramo de sua vida. Projeto que se tem um CRUD (create, read, update, delete) completo."
-                            urlGithub="https://github.com/TheRealRobinho/Integracao_Python_BD"
-                            disabled={true}
-                        />
-                    </Col>
-                    <Col md={4}>
-                        <ProjectCard
-                            image={LOL}
-                            titulo="Random LOL"
-                            descricao="O Random LOL é um projeto que lhe mostra um personagem do jogo League of Legends de forma aleatória, apresentando seu nome, ícone, habilidades, historia e características do personagem."
-                            urlGithub="https://github.com/TheRealRobinho/Random_LOL"
-                            disabled={true}
-                        />
-                    </Col>
-                    <Col md={4}>
-                        <ProjectCard
-                            image={DateCountdown}
-                            titulo="Date Countdown"
-                            descricao="O Date Countdown é um projeto que o usuário ira se informar do tempo em que há entre o agora e a data estipulada pelo mesmo. Pode ser usado, para calcular quanto tempo falta para o seu aniversário, para uma viagem, etc."
-                            urlGithub="https://github.com/TheRealRobinho/Date_Countdown"
-                            urlDemo="https://therealrobinho.github.io/Date_Countdown/"
-                        />
-                    </Col>
-                    <Col md={4}>
-                        <ProjectCard
-                            image={IMC}
-                            titulo="Calculadora IMC"
-                            descricao="A Calculadora IMC é projetada para ajudar as pessoas, dentro do padrão do IMC, identificar se esta dentro do peso ideal para suas características físicas. O IMC é calculado da seguinte forma: divide-se o peso do paciente pela sua altura elevada ao quadrado."
-                            urlGithub="https://github.com/TheRealRobinho/Random_LOL"
-                            disabled={true}
-                        />
+                        <ProjectCard />
                     </Col>
                 </Row>
             </Container>
