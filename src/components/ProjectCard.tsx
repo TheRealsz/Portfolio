3775275f46b6c92b83f57335a82ff8f257b270ac
unknown <Robindiegoa@gmail.com>
2023-05-16 22:46:04 -0300

    Adicionando props ao projectCard

--- src/components/ProjectCard.tsx
+++ src/components/ProjectCard.tsx
@@ -1,32 +1,43 @@
 import { Button } from "react-bootstrap";
 import { AiFillGithub } from "react-icons/ai";
-import { ReactNode } from 'react'
+import { SiFigma } from 'react-icons/si'
+import { ReactNode, useState } from 'react'
 import Ada from '../assets/Ada.png'
 import '../styles/projectCard.scss'
 
-// type projectCardProps = {
-//     image: string,
-//     titulo: string,
-//     descricao: string,
-//     urlGithub: string,
-//     urlDemo?: string,
-//     disabled?: boolean
-// }
+type projectCardProps = {
+    image?: string,
+    alt?: string,
+    titulo?: string,
+    descricao?: string,
+    url?: string,
+    urlDemo?: string,
+    disabled?: boolean,
+    icon?: boolean,
+    children?: ReactNode
+}
 
+// Descobrir como posso dar o set la da pagina de projetos para que mude o icone do botao
 
-export function ProjectCard(){
+export function ProjectCard(props : projectCardProps){
+    const [iconButton, setIconButton] = useState(true)
+
+    const typeOfText = iconButton ? "Github" : "Figma"
+    const typeOfbutton =<Button size="sm" className="buttonCard" href={props.url}>{iconButton? <AiFillGithub/> : <SiFigma/>}{typeOfText}</Button>
+
     return (
     <div className="project-card">
         <div className="imgProject">   
-            <img src={Ada} alt="" />
+            <img src={props.image} alt={props.alt} />
         </div>
         <div className="content">            
-                <h2>Ada</h2>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates minima esse harum blanditiis, sapiente voluptatum aut excepturi vel saepe nam et magnam nobis tempore ipsum repudiandae accusantium inventore placeat reprehenderit!</p>
+                <h2>{props.titulo}</h2>
+                <p>{props.descricao}</p>
             <div className="buttons-card">
-                <Button size="sm" className="buttonCard">GitHub</Button>
-                <Button size="sm" className="buttonCard">Demo</Button>
+                {typeOfbutton}
+                <Button size="sm" className="buttonCard" href={props.urlDemo} disabled={props.disabled}>Demo</Button>
             </div>
+            {props.children}
         </div>
     </div>
     )
