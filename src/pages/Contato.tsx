94b31a8270d29c2c3857151f28ff8790083f5b24
unknown <Robindiegoa@gmail.com>
2023-06-08 23:13:13 -0300

    Funcionalidade de envio de mensagem feita

--- src/pages/Contato.tsx
+++ src/pages/Contato.tsx
@@ -2,27 +2,32 @@ import { Button, Col, Container, Form, Row } from "react-bootstrap"
 import Particle from "../components/Particle"
 import Map from '../components/Map'
 import React, { useRef } from 'react';
-// import emailjs from '@emailjs/browser';
+import emailjs from '@emailjs/browser';
 import '../styles/contact.scss'
-
 
 // Botao disabled ate add algumas info
 // Criar outro botao para enviar? ou usar o buttonNav para dar submit tbm?
+// Pop up pra quando der tudo ok pra msg
 export function Contato() {
 
-    // const form = useRef();
 
-    // const sendEmail = (e) => {
-    //     e.preventDefault();
+    const serviceID = process.env.REACT_APP_SERVICE_ID || ''
+    const templateID = process.env.REACT_APP_TEMPLATE_ID || ''
+    const publicID = process.env.REACT_APP_PUBLIC_ID || ''
 
-    //     emailjs.sendForm('YOUR_SERVICE_ID', 'YOUR_TEMPLATE_ID', form.current, 'YOUR_PUBLIC_KEY')
-    //         .then((result) => {
-    //             console.log(result.text);
-    //         }, (error) => {
-    //             console.log(error.text);
-    //         });
-    // };
+    const form = useRef(null);
 
+    const sendEmail = (e: React.FormEvent) => {
+        e.preventDefault();
+
+        emailjs.sendForm(serviceID, templateID, form.current!, publicID)
+            .then((result) => {
+                console.log(result.text);
+            }, (error) => {
+                console.log(error.text);
+            });
+    };
+    
     return (
         <Container fluid id="contactContainer">
             <Particle />
@@ -33,25 +38,25 @@ export function Contato() {
                     </Col>
                     <Col md={7}>
                         <div className="backgroundTxt">
-                            <Form>
+                            <Form ref={form} onSubmit={sendEmail}>
                                 <h1>Entre em <span>contato</span> comigo!</h1>
                                 <p>Se deseja que eu desenvolva um projeto para voce ou apenas queira dar um oi, me mande uma mensagem!</p>
                                 <Form.Group className="mb-3" controlId="formName">
                                     <Form.Label>Nome</Form.Label>
-                                    <Form.Control type="name" placeholder="Digite seu nome" />
+                                    <Form.Control type="name" placeholder="Digite seu nome" name="name" />
                                 </Form.Group>
 
                                 <Form.Group className="mb-3" controlId="formEmail">
                                     <Form.Label>Email</Form.Label>
-                                    <Form.Control type="email" placeholder="Digite seu email" />
+                                    <Form.Control type="email" placeholder="Digite seu email" name="email"/>
                                 </Form.Group>
 
                                 <Form.Group className="mb-3" controlId="formMessage">
                                     <Form.Label>Mensagem</Form.Label>
-                                    <Form.Control as="textarea" rows={6} placeholder="Digite sua mensagem" />
+                                    <Form.Control as="textarea" rows={6} placeholder="Digite sua mensagem" name="message"/>
                                 </Form.Group>
                                 <div className="boxButtons">
-                                    <Button>Enviar</Button>
+                                    <Button type="submit">Enviar</Button>
                                     <a href="mailto:Robsondiegoandrade@outlook.com">Ou me mande um email diretamente</a>
                                 </div>
                             </Form>
